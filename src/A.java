4ee97aa3635a818125a80413f6575e6ed6ceacbf
Thothmes <117716788+Thothmes@users.noreply.github.com>
2024-12-24 16:52:58 +0500

    Delete src directory

--- src/A.java
+++
@@ -1,8 +0,0 @@
-@ToString(value = "NO")
-public class A extends Entity {
-    @ToString(value = "YES")
-    String s = "hello";
-    int x = 42;
-    @ToString(value = "NO")
-    String y = "world";
-}
